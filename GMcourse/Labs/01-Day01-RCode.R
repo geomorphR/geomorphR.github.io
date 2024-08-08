115ee01d40f0565138eb7fc4b273a461c8071e13
Dean Adams <dcadams@iastate.edu>
2024-08-08 01:27:09 -0500

    lab 1

--- GMcourse/Labs/01-Day01-RCode.R
+++ GMcourse/Labs/01-Day01-RCode.R
@@ -28,30 +28,39 @@ legend("topleft", pch=22, pt.bg = unique(gps), legend = unique(gps))
 # TPS
 M <- mshape(Y.gpa$coords)
 
-par(mfrow=c(1,2))
 plotRefToTarget(M,Y.gpa$coords[,,39], links=plethodon$links, method="vector", mag=3)
 mtext("Vector Displacements")
 plotRefToTarget(M,Y.gpa$coords[,,39], links=plethodon$links, 
                 gridPars=gridPar(pt.bg="red", link.col="green", pt.size = 1), method="vector", mag=3)
 mtext("Vector Displacements: Other Options")
-par(mfrow=c(1,1))
 
-par(mfrow=c(1,2))
+
 plotRefToTarget(M,Y.gpa$coords[,,39], mag=2, outline=plethodon$outline)  
 mtext("Outline Deformation")
 plotRefToTarget(M,Y.gpa$coords[,,39], method="points", outline=plethodon$outline)
 mtext("Outline Deformations Ref (gray) & and Tar (black)")
-par(mfrow=c(1,1))
 
 # Shape Prediction (PCA)
 
 PC <- PCA$x[,1]
 preds <- shape.predictor(Y.gpa$coords, x= PC, Intercept = FALSE, 
                          pred1 = min(PC), pred2 = max(PC)) # PC 1 extremes, more technically
-par(mfrow=c(1,2))
 plotRefToTarget(M, preds$pred1, links = plethodon$links)
 mtext("PC1 - Min.")
 plotRefToTarget(M, preds$pred2, links = plethodon$links)
 mtext("PC1 - Max.")
-par(mfrow=c(1,1))
 
+
+## Visualizing 3D shapes
+scallops <- readland.tps("Data/scallops for viz.tps", specID = "ID")
+ref <- mshape(scallops)
+refmesh <- warpRefMesh(read.ply("Data/glyp02L.ply"), 
+                       scallops[,,1], ref, color=NULL, centered=T)
+PCA.scallop <- gm.prcomp(scallops)
+plot(PCA.scallop, pch = 21, bg = "black", cex = 2)
+PC.sc <- PCA.scallop$x[,1]
+sc.preds <- shape.predictor(scallops, x= PC.sc, Intercept = FALSE, 
+                            pred1 = min(PC.sc), pred2 = max(PC.sc)) # PC 1 extremes
+
+plotRefToTarget(ref, sc.preds$pred1, mesh = refmesh, method = "surface", mag = 1)
+plotRefToTarget(ref, sc.preds$pred2, mesh = refmesh, method = "surface", mag = 1)
